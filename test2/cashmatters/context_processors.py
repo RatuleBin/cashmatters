2e0761caea7dd5b8677ac662594c6577d6086669
Narek3303 <margaryannarek056@gmail.com>
2026-02-16 16:42:46 +0600

    last changes 2

--- test2/cashmatters/context_processors.py
+++
@@ -1,9 +0,0 @@
-from blog.models import WhyCashMattersPage
-
-
-def why_cash_page(request):
-    """Add the WhyCashMattersPage to all template contexts"""
-    page = WhyCashMattersPage.objects.live().filter(slug='why-cash').first()
-    return {
-        'why_cash_page': page,
-    }
